8faa82f97b87069fdd31052b3892cf1e5d6dd3a4
Temirlan <ironone96@gmail.com>
2026-04-05 19:56:59 +0500

    test

--- js/core/geocode.js
+++ js/core/geocode.js
@@ -4,3 +4,10 @@ export async function reverseGeocode(lat, lon) {
     subtitle: `${lat.toFixed(3)}, ${lon.toFixed(3)}`,
   };
 }
+
+// export async function reversName(name) {
+//   return {
+//     title: "country",
+//     subtitle: name,
+//   };
+// }
